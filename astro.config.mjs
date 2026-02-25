9edf7c29eebbf9f9cee920b40e42d2fd0481a329
actor <gizatullininsaf25@gmail.com>
2026-02-25 16:24:17 +0300

    upd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  }
+});
